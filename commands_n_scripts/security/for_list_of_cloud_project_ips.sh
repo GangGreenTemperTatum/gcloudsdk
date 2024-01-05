90fe83d3f3539005f7525b54c3ad08f39e9cf630
Ads Dawson <104169244+GangGreenTemperTatum@users.noreply.github.com>
2024-01-05 13:53:33 -0800

    chore: for_list_of_cloud_project_ips.sh

---
+++ commands_n_scripts/security/for_list_of_cloud_project_ips.sh
@@ -0,0 +1,14 @@
+file_path="./projects_with_compute.txt"
+
+# Check if the file exists
+if [ ! -f "$file_path" ]; then
+    echo "File $file_path not found."
+    exit 1
+fi
+
+# Iterate over each line in the file and execute the command
+while IFS= read -r compute_project || [[ -n "$compute_project" ]]; do
+    command="gcloud compute addresses list --project=\"$compute_project\""
+    echo "Executing: $command"
+    eval "$command"
+done < "$file_path"
